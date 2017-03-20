7d7b4fe624f6db28acb4bc3d71700f00f408cb32
Derperinoo <derperinoo@gmail.com>
2017-03-20 19:36:30 +0800

    second i think

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
-$('#app').html(`
-	`);
-setTimeout(function(){
-	x();
-},3000);
+(function($){
+  $(function(){
 
-function x(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// $('#app').html(`<h1><img src="lawl.gif"/></h1>
+// 	<h1>Loading. . . .</h1>
+// 	`);
+
+// setTimeout(function(){
+// 	x();
+// },3000);
+
 	$.ajax({
 		url:"https://api.spotify.com/v1/search?q=artist:childish%20gambino&type=album"
 	}).done(function(response){
-		$.ajax({
+		$.ajax({	
 			url: response.albums.items[0].href
 		}).done(function(album){
 			console.log(response);
-			let name = response.albums.items[0].artists[0].name;
-			let image1 = response.albums.items[0].images[1].url;
-			let mp3_1 = album.tracks.items[0].preview_url;
-			let html = `
-			<h1>${name}</h1>
-			<img src="${image1}"/>
+			var i;
+			let html = ``;
+			for(i=0; i<6; i++){
+				html += `<div class="col s4">
+				<h4>${album.tracks.items[i].name}</h4>
+			<img src="${response.albums.items[i].images[1].url}" style="height:150px" style="width:150px"/>
 			<h1>
 			<audio controls>
-			<source src="${mp3_1}" type="audio/mpeg">
+			<source src="${album.tracks.items[i].preview_url}" type="audio/ogg">
 			Your browser does not support the audio element.
 			</audio>
 			</h1>
-		`;
+			</div>`;
+			}
+
 	$('#app').html(html); //equivalent jquery of document.getElementById("app").innerHTML=html;
 	});
 });
-}
-
-
-
+	
 
